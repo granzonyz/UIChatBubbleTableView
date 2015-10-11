6ea37fd514f625d3f87169b23deea81826f05852
Yang Zhen <ssklzs@sina.com>
2015-10-11 22:31:20 +0800

    Add github address
    Add github address

--- src/ChatBubbleMessage.swift
+++ src/ChatBubbleMessage.swift
@@ -4,6 +4,7 @@
 //
 //  Created by Yang Zhen on 15/7/4.
 //  Copyright (c) 2015 Yang Zhen. All rights reserved.
+//  GitHub: https://github.com/heavenopener/UIChatBubbleTableView
 //
 
 import Foundation
